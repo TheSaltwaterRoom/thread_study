39fbc511a8acb0c695a47aae5cedbcd28417014d
TheSaltwaterRoom <chainlockw@gmail.com>
2026-06-14 21:29:43 +0800

    feat: add first thread example

---
+++ 101first_thread/101first_thread.cpp
@@ -0,0 +1,40 @@
+﻿// 101first_thread.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <thread>
+#include <iostream>
+//Linux -lpthread
+using namespace std;
+
+void ThreadMain()
+{
+    cout << "begin sub thread main " << this_thread::get_id() << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "in thread " << i << endl;
+        this_thread::sleep_for(chrono::seconds(1));//1000ms
+    }
+    cout << "end sub thread main " << this_thread::get_id() << endl;
+}
+int main(int argc, char* argv[])
+{
+    cout << "main thread ID " << this_thread::get_id() << endl;
+    //线程创建启动
+    thread th(ThreadMain);
+    cout << "begin wait sub thread  " << endl;
+    //阻塞等待子线程退出
+    th.join();
+    cout << "end wait sub thread  " << endl;
+    return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧:
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
